478177b1ff3d3ed1d1ee72e12d0faae097fbae7e
krubro <techkru697@gmail.com>
2026-07-30 11:12:53 +0530

    First Commit

---
+++ server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+    
+}
